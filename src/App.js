387862b128fff8910f8870c2db0eabbb71c423fd
Bachry28 <muhbahryalhadi@gmail.com>
2023-10-31 12:43:47 +0700

    add reset button and status

--- src/App.js
+++ src/App.js
@@ -55,52 +55,55 @@ if (winner){
   )
 }
 
-export default function Game(){
- const [history, setHistory] = useState([Array(9).fill(null)])
- const [currentMove, setCurrentMove] = useState(0)
- const xIsNext = currentMove % 2 === 0
-  const currentSquares = history[currentMove]
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
-function jumpTo(nextMove){
-  setCurrentMove(nextMove)
-}
-
-function handlePlay(nextSquares){
-  const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
-  setHistory(nextHistory)
-  setCurrentMove(nextHistory.length - 1)
-}
-
-const moves = history.map((squares, move) => {
-  let description = ''
-  if(move > 0){
-    description = 'Go to move #' + move
-  } else {
-    description = 'Go to game start'
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
   }
 
-  return (
-    <li key={move}>
-      <button onClick={() => jumpTo(move)}>{description}</button>
-    </li>
-  )
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
 
-})
+  function handleReset() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+  }
 
+  const moves = history.map((squares, move) => {
+    let description = '';
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className='game-info'>
+        <button className="reset-button" onClick={handleReset}>
+          Reset Game
+        </button>
         <ol>{moves}</ol>
       </div>
-
     </div>
-  )
-
+  );
 }
 
 function calculateWinner(squares) {
